7886669b004fe6fcea6a1c5f150f9802cd051d2b
Jonny <jonathanriecke@gmail.com>
2020-04-07 14:22:15 -0700

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,7 +16,8 @@ class Form extends Component {
     ...DEFAULT_STATE
   }
 
-  handleSubmit() {
+  handleSubmit = (event) => {
+  
     event.preventDefault()
     document.getElementById("order-form").reset()
     this.props.addOrder(this.state)
@@ -26,13 +27,14 @@ class Form extends Component {
     })
   }
 
-  handleChange() {
+  handleChange = (event) => {
+  
     const itemType = event.target.name
     const item = event.target.value
 
     !this.state[`${itemType}`].includes(item) ?
       this.setState({
-        [itemType]: this.state[`${itemType}`].concat(item)
+        [`${itemType}`]: this.state[`${itemType}`].concat(item)
       })
     :
       this.setState({
